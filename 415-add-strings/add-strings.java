345db0be6ec566d4403bef5133b697e68688b88d
Shalvi Saxena <shalvi.saxena97@live.com>
2024-10-05 19:13:27 -0400

    Time: 2 ms (65.08%) | Memory: 42.5 MB (67.05%) - LeetSync

--- 415-add-strings/add-strings.java
+++ 415-add-strings/add-strings.java
@@ -1,21 +1,16 @@
 class Solution {
     public String addStrings(String num1, String num2) {
         char[] arr1, arr2;
-        // if(num1.length() > num2.length()) {
-            arr1 = num1.toCharArray();
-            arr2 = num2.toCharArray();
-        // } else {
-        //     arr1 = num2.toCharArray();
-        //     arr2 = num1.toCharArray();
-        // }
-        String res = "";
+        arr1 = num1.toCharArray();
+        arr2 = num2.toCharArray();
+        StringBuilder res = new StringBuilder();
         for(int i=arr1.length-1, j=arr2.length-1, c=0; i>=0 || j>=0 || c>0; i--, j--) {
             int a = i>=0? arr1[i]-'0':0;
             int b = j>=0? arr2[j]-'0':0;
             int sum = (a+b+c)%10;
-            res = sum+res;
+            res.append(sum);
             c = (a+b+c)/10;
         }
-        return res;
+        return res.reverse().toString();
     }
 }
