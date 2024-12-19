3fd9ff751aa338a26b2367be21a7815ad86b4951
Shalvi Saxena <shalvi.saxena97@live.com>
2024-12-18 20:06:49 -0500

    Time: 2 ms (41.80%) | Memory: 41.5 MB (21.88%) - LeetSync

--- 522-longest-uncommon-subsequence-ii/longest-uncommon-subsequence-ii.java
+++ 522-longest-uncommon-subsequence-ii/longest-uncommon-subsequence-ii.java
@@ -1,42 +1,35 @@
 class Solution {
-    public int findLUSlength(String[] strs) {
-        Set<String> set = new HashSet<String>();
-        Set<String> dups = new HashSet<>();
+  public int findLUSlength(String[] strs) {
+    Set<String> seen = new HashSet<>();
+    Set<String> duplicates = new HashSet<>();
 
-        Arrays.sort(strs, (a,b) -> b.length() - a.length());
-        
-        for(String str: strs) {
-            if(set.contains(str)) {
-                dups.add(str);
-            } else {
-                set.add(str);
-            }
-        }
-        // System.out.println(set);
-        
-        for(String str: strs) {
-            // System.out.println(str);
-            if(dups.contains(str)) {
-                continue;
-            }
-            boolean flag = false;
-            for(String strTemp: strs) {
-                    if(strTemp.equals(str)) break;
-                    flag = flag || checkIfSubstring(str, strTemp);
-            }
-            if(!flag) return str.length();
-            
-        }
-        return -1;
+    for (final String str : strs)
+      if (seen.contains(str))
+        duplicates.add(str);
+      else
+        seen.add(str);
+
+    Arrays.sort(strs, (a, b) -> b.length() - a.length());
+
+    for (int i = 0; i < strs.length; ++i) {
+      if (duplicates.contains(strs[i]))
+        continue;
+      boolean isASubsequence = false;
+      for (int j = 0; j < i; ++j)
+        isASubsequence |= isSubsequence(strs[i], strs[j]);
+      if (!isASubsequence)
+        return strs[i].length();
     }
 
-    boolean checkIfSubstring(String str1, String str2) {
-        int i=0,j=0;
-        for(i=0, j=0; i<str1.length() && j<str2.length(); j++) {
-            if(str1.charAt(i) == str2.charAt(j))
-                i++;
-        }
-        // System.out.println("i = "+i+" j = "+j+" str1 = "+str1+" str2 = "+str2);
-        return i==str1.length();
-    }
+    return -1;
+  }
+
+  // Returns true if a is a subsequence of b.
+  private boolean isSubsequence(final String a, final String b) {
+    int i = 0;
+    for (final char c : b.toCharArray())
+      if (i < a.length() && c == a.charAt(i))
+        ++i;
+    return i == a.length();
+  }
 }
