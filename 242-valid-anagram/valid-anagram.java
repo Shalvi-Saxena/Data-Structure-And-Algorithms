3acf48a26caf33c68ffc5188f925993a9f62c4cc
Shalvi Saxena <shalvi.saxena97@live.com>
2025-03-31 18:47:38 -0400

    Time: 24 ms (8.44%) | Memory: 42.9 MB (90.26%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,15 +1,20 @@
+import java.io.*;
+
 class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length() != t.length()) {
             return false;
         }
-        char[] sArr = s.toCharArray();
-        char[] tArr = t.toCharArray();
-        Arrays.sort(sArr);
-        Arrays.sort(tArr);
+        HashMap<Character, Integer> hm = new HashMap<>();
 
         for(int i=0; i<s.length(); i++) {
-            if(sArr[i] != tArr[i]) {
+            hm.put(s.charAt(i), hm.getOrDefault(s.charAt(i), 0) + 1 );
+            hm.put(t.charAt(i), hm.getOrDefault(t.charAt(i), 0) - 1 );
+        }
+        // System.out.print(hm.keySet());
+        // System.out.print(hm.values());
+        for(int i: hm.values()) {
+            if(i!=0) {
                 return false;
             }
         }
