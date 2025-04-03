fbd4a8d7a98af5f6c277ab342073bc5b09646ed6
Shalvi Saxena <shalvi.saxena97@live.com>
2025-04-02 23:33:37 -0400

    Time: 0 ms (100.00%) | Memory: 44.6 MB (49.81%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.java
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -11,23 +11,18 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        // System.out.println("head "+head.val);
         if(head == null || head.next == null) {
             return false;
         }
         ListNode fast = head, slow = head;
 
         while(fast != null && slow != null) {
-            // System.out.println("Fast "+fast.val +". slow "+slow.val);
             fast = fast.next != null? fast.next.next : fast.next;
             slow = slow.next;
             if(fast == slow)
                 return true;
         }
 
-        if( fast == null || slow == null) {
-            return false;
-        }
-        return true;
+        return false;
     }
 }
