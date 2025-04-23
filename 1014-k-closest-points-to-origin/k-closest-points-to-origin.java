83b709ce4c20905493ae44f34cdda690b9289e6c
Shalvi Saxena <shalvi.saxena97@live.com>
2025-04-23 08:23:12 -0400

    Time: 16 ms (93.54%) | Memory: 55.5 MB (18.77%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -1,6 +1,21 @@
 class Solution {
+    public int getSqrDist(int[] point) {
+        return point[0]*point[0] + point[1]*point[1];
+    }
     public int[][] kClosest(int[][] points, int k) {
-        Arrays.sort(points, Comparator.comparing(p -> p[0] * p[0] + p[1] * p[1]));
-        return Arrays.copyOfRange(points, 0, k);
+        PriorityQueue<int[]> heap = new PriorityQueue<>((a,b) -> getSqrDist(b) - getSqrDist(a));
+        int i=0;
+        for(int[] point: points) {
+            if(i++<k) {
+                heap.offer(point);
+            } else {
+                if(getSqrDist(point) < getSqrDist(heap.peek())) {
+                    heap.poll();
+                    heap.offer(point);
+                }
+            }
+        }
+
+        return heap.toArray(new int[k][]);
     }
 }
