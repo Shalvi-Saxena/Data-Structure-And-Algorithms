4675cff1ed2b42611c12ac86bfbb32c0e5a777bf
Shalvi Saxena <shalvi.saxena97@live.com>
2025-04-23 21:24:16 -0400

    Time: 5 ms (97.63%) | Memory: 44.9 MB (42.98%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
@@ -1,29 +1,32 @@
 class Solution {
-    public int getValue(String op, String b, String a) {
-        int a1 = Integer.valueOf(a), b1 = Integer.valueOf(b);
-        switch(op) {
-            case "+":
-                return a1+b1;
-            case "-":
-                return a1-b1;
-            case "*":
-                return a1*b1;
-            default:
-                return a1/b1;
-        }
-    }
     public int evalRPN(String[] tokens) {
-        Stack<String> exp = new Stack<>();
+        Stack<Integer> exp = new Stack<>();
+        int a,b;
 
         for(String token: tokens) {
-            if(exp.isEmpty()) {
-                exp.push(token);
-            } else {
-                if(token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/")) {
-                    exp.push(String.valueOf(getValue(token, exp.pop(), exp.pop())));
-                } else {
-                    exp.push(token);
-                }
+            switch(token) {
+                case "+":
+                    b = exp.pop();
+                    a = exp.pop();
+                    exp.push(a+b);
+                    break;
+                case "-":
+                    b = exp.pop();
+                    a = exp.pop();
+                    exp.push(a-b);
+                    break;
+                case "/":
+                    b = exp.pop();
+                    a = exp.pop();
+                    exp.push(a/b);
+                    break;
+                case "*":
+                    b = exp.pop();
+                    a = exp.pop();
+                    exp.push(a*b);
+                    break;
+                default:
+                    exp.push(Integer.parseInt(token));
             }
         }
         return Integer.valueOf(exp.peek());
