4632d2547c6eadf5b29f645cfda50a9f9585070b
Shalvi Saxena <shalvi.saxena97@live.com>
2025-04-28 18:37:25 -0400

    Time: 2 ms (82.94%) | Memory: 44.7 MB (46.72%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,14 +1,14 @@
 class Solution {
-    List<List<Integer>> res;
     public List<List<Integer>> findCombination(int[] candidates, int target, int sum, List<Integer> path, int i, List<List<Integer>> res) {
         if(sum == target) {
             res.add(new ArrayList<>(path));
             // System.out.print("sum = "+sum+" path = "+path+" res = "+res);
             return res;
         }
-        if(sum>target || i>=candidates.length) return res;
+        if(sum>target) return res;
 
         for(int j = i; j<candidates.length; j++) {
+            if(sum+candidates[j] > target)  continue;
             path.add(candidates[j]);
             res = findCombination(candidates, target, sum+candidates[j], path, j, res);
             path.remove(Integer.valueOf(candidates[j]));
@@ -16,7 +16,7 @@ class Solution {
         return res;
     }
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        res = new ArrayList<>();
+        List<List<Integer>> res = new ArrayList<>();
         return findCombination(candidates, target, 0, new ArrayList<Integer>(), 0, res);
     }
 }
