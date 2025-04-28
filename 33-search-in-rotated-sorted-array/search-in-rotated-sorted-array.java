21663ddd9e9ea6af1ced35802988aeca82e5fc6f
Shalvi Saxena <shalvi.saxena97@live.com>
2025-04-28 12:24:36 -0400

    Time: 12 ms (2.19%) | Memory: 43 MB (6.71%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -2,16 +2,24 @@ class Solution {
     public int binarySearch(int[] nums, int start, int end, int target) {
         int mid = start + ((end-start)/2);
 
+        System.out.println("start = "+start+" mid = "+mid+" end = "+end);
+
         if(start > end) {
             return -1;
         }
         if(nums[mid] == target) {
             return mid;
-        } else if(nums[mid] > target) {
+        } else if(nums[mid] < target && nums[end] >= target) {
+            return binarySearch(nums, mid+1, end, target);
+        } else if(nums[mid] < target && nums[end] <= target) {
             return binarySearch(nums, start, mid-1, target);
+        } else if(nums[mid] < target && nums[end] <= target) {
+            return binarySearch(nums, start, mid-1, target);
+        } else if(nums[mid] > target && nums[start] <= target) {
+            return binarySearch(nums, start, mid-1, target);
+        } else {
+            return binarySearch(nums, mid+1, end, target);
         }
-
-        return binarySearch(nums, mid+1, end, target);
     }
 
     public int search(int[] nums, int target) {
