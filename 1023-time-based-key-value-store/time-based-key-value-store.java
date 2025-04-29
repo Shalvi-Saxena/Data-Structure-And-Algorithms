e4b742293ea84474d3e5cc9f95516b81cd8afa0c
Shalvi Saxena <shalvi.saxena97@live.com>
2025-04-29 08:06:31 -0400

    Time: 141 ms (62.23%) | Memory: 118 MB (98.14%) - LeetSync

--- 1023-time-based-key-value-store/time-based-key-value-store.java
+++ 1023-time-based-key-value-store/time-based-key-value-store.java
@@ -1,43 +1,42 @@
 class TimeMap {
-    HashMap<String, List<Pair<Integer, String>>> map;
+    static class Entry {
+        int time;
+        String value;
+        Entry(int time, String value) {
+            this.time = time;
+            this.value = value;
+        }
+    }
+
+    HashMap<String, List<Entry>> map;
 
     public TimeMap() {
         map = new HashMap<>();
     }
-    
+
     public void set(String key, String value, int timestamp) {
-        Pair<Integer, String> pair = new Pair<>(timestamp, value);
-        List<Pair<Integer, String>> list = map.getOrDefault(key, new ArrayList<>());
-        list.add(pair);
-        map.put(key, list);
+        map.computeIfAbsent(key, k -> new ArrayList<>()).add(new Entry(timestamp, value));
     }
-    
+
     public String get(String key, int timestamp) {
-        if(map.containsKey(key)) {
-            List<Pair<Integer, String>> list = map.get(key);
-            String res = "";
-            int start = 0, end = list.size()-1;
-            while(start<=end) {
-                int mid = start+(end-start)/2;
-                if(list.get(mid).getKey() == timestamp) {
-                    return list.get(mid).getValue();
-                }
-                if(list.get(mid).getKey() < timestamp) {
-                    res = list.get(mid).getValue();
-                    start = mid+1;
-                } else if(list.get(mid).getKey() > timestamp) {
-                    end = mid-1;
-                }
+        if (!map.containsKey(key)) return "";
+
+        List<Entry> list = map.get(key);
+        int left = 0, right = list.size() - 1;
+        String res = "";
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (list.get(mid).time == timestamp)
+                return list.get(mid).value;
+            else if (list.get(mid).time < timestamp) {
+                res = list.get(mid).value;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
-            return res;
         }
-        return "";
+
+        return res;
     }
 }
-
-/**
- * Your TimeMap object will be instantiated and called as such:
- * TimeMap obj = new TimeMap();
- * obj.set(key,value,timestamp);
- * String param_2 = obj.get(key,timestamp);
- */
