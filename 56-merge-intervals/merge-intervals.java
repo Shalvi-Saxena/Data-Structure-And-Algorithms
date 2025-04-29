edcb0d1149f16efc71139d16e9bd1c9a2d4f3577
Shalvi Saxena <shalvi.saxena97@live.com>
2025-04-29 18:53:38 -0400

    Time: 9 ms (48.65%) | Memory: 46.6 MB (54.89%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,27 +1,19 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        if(intervals.length == 1)   return intervals;
+        if (intervals.length <= 1) return intervals;
 
-        Arrays.sort(intervals, (a, b)-> a[0]-b[0]);
-        List<int[]> arr = new ArrayList<>();
-        for(int[] i: intervals) {
-            // System.out.println(i[0]+".  "+i[1]);
-            if(arr.isEmpty()) {
-                arr.add(i);
-                continue;
-            }
-            if(arr.get(arr.size()-1)[1] >= i[0]) {
-                arr.get(arr.size()-1)[1] = Math.max(arr.get(arr.size()-1)[1], i[1]);
-                arr.get(arr.size()-1)[0] = Math.min(arr.get(arr.size()-1)[0], i[0]);
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+
+        List<int[]> merged = new ArrayList<>();
+
+        for (int[] interval : intervals) {
+            if (merged.isEmpty() || merged.get(merged.size() - 1)[1] < interval[0]) {
+                merged.add(interval);
             } else {
-                arr.add(i);
+                merged.get(merged.size() - 1)[1] = Math.max(merged.get(merged.size() - 1)[1], interval[1]);
             }
         }
-        int[][] finalIntervals = new int[arr.size()][2];
-        int i=0;
-        for(int[] item: arr) {
-            finalIntervals[i++] = item;
-        }
-        return finalIntervals;
+
+        return merged.toArray(new int[0][]);
     }
 }
