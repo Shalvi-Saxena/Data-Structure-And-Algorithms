e5c16b0f4c1b390d8372bea3c289660bfcad7acd
Shalvi Saxena <shalvi.saxena97@live.com>
2025-04-28 20:46:02 -0400

    Time: 2 ms (39.61%) | Memory: 45 MB (6.03%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,21 +1,19 @@
 class Solution {
-    public List<List<Integer>> findComb(int[] nums, List<Integer> combination, HashSet<Integer> queue, List<List<Integer>> combinations) {
+    public List<List<Integer>> findComb(int[] nums, List<Integer> combination, List<List<Integer>> combinations) {
         if(combination.size() == nums.length) {
             combinations.add(new ArrayList<Integer>(combination));
             return combinations;
         }
         for(int i=0; i<nums.length; i++) {
-            if(queue.contains(nums[i]))   continue;
+            if(combination.contains(nums[i]))   continue;
             combination.add(nums[i]);
-            queue.add(nums[i]);
-            combinations = findComb(nums, combination, queue, combinations);
+            combinations = findComb(nums, combination, combinations);
             combination.remove(combination.size() - 1);
-            queue.remove(nums[i]);
         }
 
         return combinations;
     }
     public List<List<Integer>> permute(int[] nums) {
-        return findComb(nums, new ArrayList<Integer>(), new HashSet<Integer>(), new ArrayList<>());
+        return findComb(nums, new ArrayList<Integer>(), new ArrayList<>());
     }
 }
