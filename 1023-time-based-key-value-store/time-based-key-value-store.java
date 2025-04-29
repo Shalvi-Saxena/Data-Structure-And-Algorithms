4a04f8a79aeaa5fce4a1effd5903ce5a4997db94
Shalvi Saxena <shalvi.saxena97@live.com>
2025-04-29 08:04:10 -0400

    Time: 140 ms (64.96%) | Memory: 119.7 MB (59.49%) - LeetSync

--- 1023-time-based-key-value-store/time-based-key-value-store.java
+++ 1023-time-based-key-value-store/time-based-key-value-store.java
@@ -15,27 +15,23 @@ class TimeMap {
     public String get(String key, int timestamp) {
         if(map.containsKey(key)) {
             List<Pair<Integer, String>> list = map.get(key);
-            return binarySearch(map.get(key), timestamp, 0, list.size()-1);
+            String res = "";
+            int start = 0, end = list.size()-1;
+            while(start<=end) {
+                int mid = start+(end-start)/2;
+                if(list.get(mid).getKey() == timestamp) {
+                    return list.get(mid).getValue();
+                }
+                if(list.get(mid).getKey() < timestamp) {
+                    res = list.get(mid).getValue();
+                    start = mid+1;
+                } else if(list.get(mid).getKey() > timestamp) {
+                    end = mid-1;
+                }
+            }
+            return res;
         }
         return "";
-    }
-
-    public String binarySearch(List<Pair<Integer, String>> list, int timestamp, int start, int end) {
-        String res = "";
-        while(start<=end) {
-        // System.out.println("start = "+start+" end = "+end+" timestamp = "+timestamp);
-            int mid = start+(end-start)/2;
-            if(list.get(mid).getKey() == timestamp) {
-                return list.get(mid).getValue();
-            }
-            if(list.get(mid).getKey() < timestamp) {
-                res = list.get(mid).getValue();
-                start = mid+1;
-            } else if(list.get(mid).getKey() > timestamp) {
-                end = mid-1;
-            }
-        }
-        return res;
     }
 }
 
