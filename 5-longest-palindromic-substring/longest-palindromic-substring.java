802680efdb442da005d0f9d5e1fb8a57a3f267d1
Shalvi Saxena <shalvi.saxena97@live.com>
2025-04-30 08:35:03 -0400

    Time: 7 ms (97.35%) | Memory: 42.4 MB (73.93%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,40 +1,32 @@
 class Solution {
     public String longestPalindrome(String s) {
-        Pair<Integer, Integer> res = new Pair(0, 0);
-        int lenRes = 1;
-        
-        for(int i=0; i<s.length(); i++) {
-            Pair<Integer, Integer> even = expand(s, i, i+1);
-            int lenEven = even.getValue()-even.getKey()+1;
-            
-            Pair<Integer, Integer> odd = expand(s, i, i);
-            int lenOdd = odd.getValue()-odd.getKey()+1;
-            
-            if(lenEven > lenOdd && lenEven > lenRes) {
-                res = even;
-                lenRes = lenEven;
-            } else if(lenEven < lenOdd && lenOdd > lenRes) {
-                res = odd;
-                lenRes = lenOdd;
+        if(s.length() <= 1)   return s;
+        int start=0, maxLength=1;
+        char[] str = s.toCharArray();
+        for(int i=0; i<str.length-1; i++) {
+            // Check for odd length
+            int low = i-1, high = i+1;
+            while(low >= 0 && high < str.length && str[low] == str[high]) {
+                if(high-low+1 > maxLength) {
+                    start = low;
+                    maxLength = high-low+1;
+                }
+                low--;
+                high++;
             }
-            // System.out.println(i+"  "+even+"  "+lenEven+"  "+odd+"  "+lenOdd+"  "+res+"  "+lenRes);
-            
+            // Check for even length
+            if(str[i] == str[i+1]) {
+                low = i; high = i+1;
+                while(low >= 0 && high < str.length && str[low] == str[high]) {
+                    if(high-low+1 > maxLength) {
+                        start = low;
+                        maxLength = high-low+1;
+                    }
+                    low--;
+                    high++;
+                }
+            }
         }
-        return s.substring(res.getKey(), res.getValue()+1);
-    }
-    
-    public Pair expand(String s, int left, int right) {
-        int l=left, r=right;
-        
-        if(r>=s.length() || s.charAt(l) != s.charAt(r)) return new Pair(l, l);
-        
-        while(l>0 && r<(s.length()-1) ) {
-            if(s.charAt(l-1) == s.charAt(r+1)) {
-                l--;
-                r++;
-            } else break;
-        }
-        
-        return new Pair<Integer, Integer>(l, r);
+        return s.substring(start, start+maxLength);
     }
 }
