093dbee42ef514cfb4a14fad8abe0dce6820b8c1
Shalvi Saxena <shalvi.saxena97@live.com>
2025-05-01 17:15:32 -0400

    Time: 0 ms (100.00%) | Memory: 43.1 MB (10.42%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,17 +1,21 @@
 class Solution {
-    public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> subsets = new ArrayList<>();
-        subsets.add(new ArrayList<>());
-        for(int i=0; i<nums.length; i++) {
-            List<List<Integer>> newSubsets = new ArrayList<>();
-            for(List subset: subsets) {
-                List<Integer> tempSet = new ArrayList<Integer> (subset);
-                tempSet.add(nums[i]);
-                newSubsets.add(tempSet);
-            }
-            subsets.addAll(newSubsets);
+    public void createSubsets(int[] nums, int i, List<Integer> subset, List<List<Integer>> superSet) {
+        if(nums.length == i) {
+            superSet.add(new ArrayList<Integer>(subset));
+            return;
         }
 
-        return subsets;
+        subset.add(nums[i]);
+        createSubsets(nums, i+1, subset, superSet);
+        subset.remove(subset.size()-1);
+        createSubsets(nums, i+1, subset, superSet);
+
+        return;
+    }
+
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> superSet = new ArrayList<>();
+        createSubsets(nums, 0, new ArrayList<Integer>(), superSet);
+        return superSet;
     }
 }
