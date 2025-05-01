207dda1a56800ea64262d29b4b8ff60f077e6952
Shalvi Saxena <shalvi.saxena97@live.com>
2025-05-01 14:48:19 -0400

    Time: 35 ms (28.60%) | Memory: 57.2 MB (32.83%) - LeetSync

--- 310-minimum-height-trees/minimum-height-trees.java
+++ 310-minimum-height-trees/minimum-height-trees.java
@@ -1,29 +1,34 @@
 class Solution {
     public List<Integer> findMinHeightTrees(int n, int[][] edges) {
+        List<Integer> res = new ArrayList<>();
         if(n==1) {
-            return Collections.singletonList(0);
+            res.add(0);
+            return res;
         }
         List<Set<Integer>> graph = new ArrayList<>();
-        List<Integer> res = new ArrayList<>();
+        int[] degree = new int[n];
         for (int i = 0; i < n; i++) {
             graph.add(new HashSet<Integer>());
         }
 
         for (int[] edge : edges) {
+            degree[edge[0]]++;  degree[edge[1]]++;
             graph.get(edge[0]).add(edge[1]);
             graph.get(edge[1]).add(edge[0]);
         }
 
         List<Integer> leafNodes = new ArrayList<>();
         for (int i = 0; i < n; i++) {
-            if (graph.get(i).size() == 1) {
+            if (degree[i] == 1) {
                 leafNodes.add(i);
             }
         }
 
-        while(!leafNodes.isEmpty()) {
-            res = leafNodes;
+        int remainingNodes = n;
+
+        while(n > 2) {
             List<Integer> newLeafNodes = new ArrayList<>();
+            n -= leafNodes.size();
 
             for(int node: leafNodes) {
                 if(graph.get(node).size() == 1) {
@@ -38,6 +43,6 @@ class Solution {
             leafNodes = newLeafNodes;
         }
 
-        return res;
+        return leafNodes;
     }
 }
