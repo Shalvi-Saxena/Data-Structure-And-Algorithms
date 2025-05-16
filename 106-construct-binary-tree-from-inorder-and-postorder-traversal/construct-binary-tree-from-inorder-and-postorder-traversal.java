99f5462de84dc8754208f04ff100df9efb59197c
Shalvi Saxena <shalvi.saxena97@live.com>
2025-05-16 15:31:02 -0400

    Time: 1 ms (97.91%) | Memory: 44.3 MB (78.46%) - LeetSync

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.java
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -15,27 +15,23 @@
  */
 class Solution {
     int index;
-    public int getIndex(int[] inorder, int val, int start, int end) {
-        for(int i=start; i<=end; i++) {
-            if(val == inorder[i]) {
-                return i;
-            }
-        }
-        return -1;
-    }
+    HashMap<Integer, Integer> inIndex;
     public TreeNode buildTree(int[] inorder, int[] postorder,int start, int end) {
-        // System.out.println("start = "+start+" end = "+end+" index = "+index);
         if(start>end || index<0) {
             return null;
         }
         TreeNode root = new TreeNode(postorder[index--]);
-        int newIndex = getIndex(inorder, root.val, start, end);
+        int newIndex = inIndex.get(root.val);
         root.right = buildTree(inorder, postorder, newIndex+1, end);
         root.left = buildTree(inorder, postorder, start, newIndex-1);
         return root;
     }
     public TreeNode buildTree(int[] inorder, int[] postorder) {
         index = inorder.length-1;
+        inIndex = new HashMap<>();
+        for(int i=0; i<inorder.length; i++) {
+            inIndex.put(inorder[i], i);
+        }
         return buildTree(inorder, postorder, 0, index);
     }
 }
