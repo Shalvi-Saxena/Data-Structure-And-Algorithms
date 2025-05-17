ad6b1709d1a6b97f817388eb75825a2cf1eba39b
Shalvi Saxena <shalvi.saxena97@live.com>
2025-05-16 23:02:56 -0400

    Time: 37 ms (63.82%) | Memory: 62.1 MB (29.40%) - LeetSync

--- 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.java
+++ 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.java
@@ -1,37 +1,33 @@
-import java.util.*;
-
 class Solution {
     public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
-        int n = startTime.length;
-        int[][] jobs = new int[n][3];
-        
-        for (int i = 0; i < n; i++) {
-            jobs[i] = new int[] {startTime[i], endTime[i], profit[i]};
+        int[][] jobs = new int[startTime.length][3];
+        for(int i=0; i<startTime.length; i++) {
+            jobs[i] = new int[]{startTime[i], endTime[i], profit[i]};
         }
-        Arrays.sort(jobs, (a, b) -> a[0] - b[0]);
-        Integer[] dp = new Integer[n];
-        return dfs(0, jobs, dp);
+        Arrays.sort(jobs, (a,b)-> a[0]-b[0]);
+        return dfs(jobs, new int[startTime.length], 0);
     }
 
-    private int dfs(int i, int[][] jobs, Integer[] dp) {
-        if (i == jobs.length) return 0;
-        if (dp[i] != null) return dp[i];
-        int nextIndex = binarySearch(jobs, jobs[i][1]);
-        int take = jobs[i][2] + dfs(nextIndex, jobs, dp);
-        int skip = dfs(i + 1, jobs, dp);
-        return dp[i] = Math.max(take, skip);
+    public int dfs(int[][] jobs, int[] dp, int i) {
+        if(i >= dp.length)  return 0;
+        if(dp[i] != 0)  return dp[i];
+        int next = findNext(jobs, jobs[i][1]);
+        int take = jobs[i][2]+dfs(jobs, dp, next);
+        int skip = dfs(jobs, dp, i+1);
+        dp[i] = Math.max(take, skip);
+        return dp[i];
     }
 
-    private int binarySearch(int[][] jobs, int target) {
-        int low = 0, high = jobs.length;
-        while (low < high) {
-            int mid = low + (high - low) / 2;
-            if (jobs[mid][0] < target) {
-                low = mid + 1;
+    public int findNext(int[][] jobs, int target) {
+        int start=0, end = jobs.length;
+        while(start<end) {
+            int mid = start+(end-start)/2;
+            if(jobs[mid][0]>=target) {
+                end = mid;
             } else {
-                high = mid;
+                start = mid+1;
             }
         }
-        return low;
+        return start;
     }
 }
