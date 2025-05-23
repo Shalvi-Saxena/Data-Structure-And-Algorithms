76b15074115b24f4d83098fbfcf3f6ccd2fc945b
Shalvi Saxena <shalvi.saxena97@live.com>
2025-05-23 17:41:26 -0400

    Time: 1 ms (99.34%) | Memory: 42.9 MB (67.46%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -1,26 +1,26 @@
 class Solution {
-    class Trie{
+    class Trie {
         Trie[] level;
         boolean isEOW;
+
         Trie() {
             level = new Trie[26];
             isEOW = false;
         }
     }
 
-    public void add(char[] str, Trie root) {
+    public void insertWord(char[] str, Trie root) {
         Trie curr = root;
-        for(int i=0; i<str.length; i++) {
-            int index = str[i]-'a';
-            if(curr.level[index] == null) {
-                curr.level[index] = new Trie();
+        for(char ch: str) {
+            if(curr.level[ch-'a'] == null) {
+                curr.level[ch-'a'] = new Trie();
             }
-            curr = curr.level[index];
+            curr = curr.level[ch-'a'];
         }
         curr.isEOW = true;
     }
 
-    public boolean search(char[] str, Trie root, int i, Boolean[] dp) {
+    public boolean searchWord(char[] str, Trie root, int i, Boolean[] dp) {
         if(i==str.length) {
             return true;
         }
@@ -28,24 +28,24 @@ class Solution {
             return dp[i];
         }
         Trie curr = root;
-        for (int j = i; j < str.length; j++) {
-            int index = str[j] - 'a';
-            if (curr.level[index] == null) break;
-            curr = curr.level[index];
-            if (curr.isEOW && search(str, root, j+1, dp)) {
-                return dp[i] = true;
+        for(int j=i; j<str.length; j++) {
+            int ind = str[j]-'a';
+            if(curr.level[ind] == null) {
+                break;
+            }
+            curr = curr.level[ind];
+            if(curr.isEOW && searchWord(str, root, j+1, dp)) {
+                return dp[j] = true;
             }
         }
-
         return dp[i] = false;
     }
 
     public boolean wordBreak(String s, List<String> wordDict) {
-        Trie trie = new Trie();
+        Trie root = new Trie();
         for(String str: wordDict) {
-            add(str.toCharArray(), trie);
+            insertWord(str.toCharArray(), root);
         }
-        char[] str = s.toCharArray();
-        return search(str, trie, 0, new Boolean[str.length]);
+        return searchWord(s.toCharArray(), root, 0, new Boolean[s.length()]);
     }
 }
