b4b08967e47d93acb11136cd9f8806b5fba66e92
Shalvi Saxena <shalvi.saxena97@live.com>
2025-05-23 19:58:18 -0400

    Time: 0 ms (100.00%) | Memory: 42 MB (68.10%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,31 +1,41 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int i,j;
-        for(i=0, j=nums.length-1; i<nums.length && j>i; i++) {
-            if(nums[i] != 0) {
-                while(j>i && nums[j] != 0) {
-                    j--;
-                }
-                if(i!=j) {
-                    nums[j] = nums[i];
-                    nums[i] = 0;
-                }
-                // System.out.println("nums "+Arrays.toString(nums)+" i = "+i+" j = "+j);
+        int left = 0, right = nums.length-1;
+
+        while(left < right) {
+            while(left<right && nums[left] == 0) {
+                left++;
+            } 
+            while(left<right && nums[right] != 0) {
+                right--;
+            }
+            if(left < right) {
+                int num = nums[left];
+                nums[left] = nums[right];
+                nums[right] = num;
+                left++;
+                right--;
             }
         }
-        i = 0;
-        while(i<nums.length && nums[i] == 0) i++;
-        // System.out.println("nums "+Arrays.toString(nums)+" i = "+i);
-        for(j=nums.length-1; i<nums.length && j>i; i++) {
-           if(nums[i] != 1) {
-                while(j>i && nums[j] != 1) {
-                    j--;
-                }
-                if(j>i) {
-                    nums[j] = nums[i];
-                    nums[i] = 1;
-                }
+        right = nums.length-1;
+        left=0;
+
+        while(left<right && nums[left] == 0)  left++;
+
+        while(left < right) {
+            while(left<right && nums[left] == 1) {
+                left++;
             } 
+            while(left<right && nums[right] != 1) {
+                right--;
+            }
+            if(left < right) {
+                int num = nums[left];
+                nums[left] = nums[right];
+                nums[right] = num;
+                left++;
+                right--;
+            }
         }
     }
 }
