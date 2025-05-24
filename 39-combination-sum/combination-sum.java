50fc45cf36bf7f6f318ce04dd209377c8f07a25c
Shalvi Saxena <shalvi.saxena97@live.com>
2025-05-24 09:04:34 -0400

    Time: 2 ms (83.41%) | Memory: 44.2 MB (98.52%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,22 +1,21 @@
 class Solution {
-    public List<List<Integer>> findCombination(int[] candidates, int target, int sum, List<Integer> path, int i, List<List<Integer>> res) {
+    List<List<Integer>> res;
+    public void getCombinations(int[] candidates, int target, List<Integer> comb, int i, int sum) {
+        if(sum > target || i >= candidates.length)    return;
         if(sum == target) {
-            res.add(new ArrayList<>(path));
-            // System.out.print("sum = "+sum+" path = "+path+" res = "+res);
-            return res;
+            res.add(new ArrayList<>(comb));
+            return;
         }
-        if(sum>target) return res;
-
-        for(int j = i; j<candidates.length; j++) {
-            if(sum+candidates[j] > target)  continue;
-            path.add(candidates[j]);
-            res = findCombination(candidates, target, sum+candidates[j], path, j, res);
-            path.remove(path.size()-1);
+        for(int j=i; j<candidates.length; j++) {
+            comb.add(candidates[j]);
+            getCombinations(candidates, target, comb, j, sum+candidates[j]);
+            comb.remove(comb.size()-1);
         }
-        return res;
+        return;
     }
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> res = new ArrayList<>();
-        return findCombination(candidates, target, 0, new ArrayList<Integer>(), 0, res);
+        res = new ArrayList<>();
+        getCombinations(candidates, target, new ArrayList<>(), 0, 0);   
+        return res;
     }
 }
