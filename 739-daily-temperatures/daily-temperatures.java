edf53f381c4b82036d2876a5a9aae7251cc6627b
Shalvi Saxena <shalvi.saxena97@live.com>
2025-06-08 20:15:51 -0400

    Time: 79 ms (27.32%) | Memory: 60.4 MB (61.03%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.java
+++ 739-daily-temperatures/daily-temperatures.java
@@ -2,21 +2,18 @@ class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
         int n = temperatures.length;
         int[] waitTime = new int[n];
-        waitTime[n-1] = 0;
         Stack<Integer> max = new Stack<>();
-        max.push(n-1);
 
-        for(int i=n-2; i>=0; i--) {
+        for(int i=n-1; i>=0; i--) {
             while(!max.isEmpty() && temperatures[max.peek()] <= temperatures[i]) {
                 max.pop();
             }
             if(max.isEmpty()) {
-                max.push(i);
                 waitTime[i] = 0;
             } else {
                 waitTime[i] = max.peek()-i;
-                max.push(i);
             }
+            max.push(i);
         }
         
         return waitTime;
