2ba5e4bed60ab0c235e34fd4a0b90177518588a1
Shalvi Saxena <shalvi.saxena97@live.com>
2025-09-24 21:08:55 -0400

    Time: 0 ms (100.00%) | Memory: 44.9 MB (97.50%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,21 +1,11 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int max = 0;
-        for(int i: nums) {
-            max = Math.max(i, max);
-        }
-        boolean[] arr = new boolean[max+1];
-
-        for(int i: nums) {
-            arr[i] = true;
+        int sum1 = 0, sum2 = nums.length, n = nums.length;
+        for(int i=0; i<n; i++) {
+            sum1 += nums[i];
+            sum2 += i;
         }
 
-        for(int i=0; i<=max; i++) {
-            if(!arr[i]) {
-                return i;
-            }
-        }
-
-        return max+1;
+        return sum2-sum1;
     }
 }
