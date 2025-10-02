912fd979750f0f920e94a091b334c209c3320d34
Shalvi Saxena <shalvi.saxena97@live.com>
2025-10-02 11:19:58 -0400

    Time: 2 ms (98.11%) | Memory: 44.6 MB (42.01%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.java
+++ 572-subtree-of-another-tree/subtree-of-another-tree.java
@@ -14,22 +14,11 @@
  * }
  */
 class Solution {
-    public TreeNode findNode(TreeNode root, TreeNode subRoot) {
-        if(root == null || root.val == subRoot.val) {
-            return root;
-        }
-        TreeNode leftNode = findNode(root.left, subRoot);
-        if(leftNode != null && leftNode.val == subRoot.val) {
-            return leftNode;
-        }
-        return findNode(root.right, subRoot);
-    }
-
     public boolean checkSubTree(TreeNode root, TreeNode subRoot) {
         if(root == null && subRoot == null) {
             return true;
         }
-        if((root == null && subRoot != null) || (root != null && subRoot == null) ||
+        if((root == null || subRoot == null) ||
            (root.val != subRoot.val)) {
             return false;
         }
@@ -37,22 +26,16 @@ class Solution {
         checkSubTree(root.right, subRoot.right);
     }
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        Stack<TreeNode> list = new Stack<>();
-        list.push(root);
-        while(!list.isEmpty()) {
-            TreeNode node = list.pop();
-            if(node.val == subRoot.val && checkSubTree(node, subRoot)) {
-                return true;
-            }
-            if(node.left != null) {
-                list.add(node.left);
-            }
-
-            if(node.right != null) {
-                list.add(node.right);
-            }
+        if(root == null && subRoot == null) {
+            return true;
+        }
+        if(root == null || subRoot == null) {
+            return false;
         }
 
-        return false;
+        if(root.val == subRoot.val && checkSubTree(root, subRoot)) {
+            return true;
+        }
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
     }
 }
