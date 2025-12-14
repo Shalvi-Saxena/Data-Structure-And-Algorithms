d64c2923644d6a6bdea1141fcb21ca9c0a462d8a
Shalvi Saxena <shalvi.saxena97@live.com>
2025-12-14 18:04:31 -0500

    Time: 7 ms (80.95%) | Memory: 46.5 MB (47.07%) - LeetSync

---
+++ 692-top-k-frequent-words/top-k-frequent-words.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<String> topKFrequent(String[] words, int k) {
+        Map<String, Integer> dict = new HashMap<>();
+        
+        for(String word: words) {
+            dict.put(word, dict.getOrDefault(word, 0)+1);
+        }
+
+        PriorityQueue<Map.Entry<String, Integer>> pq = new PriorityQueue<>(
+            (a,b) -> a.getValue().equals(b.getValue())? b.getKey().compareTo(a.getKey()) : a.getValue()-b.getValue()
+        );
+        
+        for (Map.Entry<String, Integer> entry : dict.entrySet()) {
+            pq.offer(entry);
+            if(pq.size()>k)
+                pq.poll();
+        }
+
+        List<String> res = new LinkedList<>();
+        while(!pq.isEmpty()) {
+            res.add(0, pq.poll().getKey());
+        }
+
+        return res;
+    }
+}
