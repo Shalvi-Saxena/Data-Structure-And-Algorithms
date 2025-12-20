459c5cfef910c302ce7614021010f1aacb41cf76
Shalvi Saxena <shalvi.saxena97@live.com>
2025-12-20 08:28:12 -0500

    Time: 4 ms (98.03%) | Memory: 48.1 MB (10.46%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.java
+++ 658-find-k-closest-elements/find-k-closest-elements.java
@@ -1,35 +1,20 @@
 class Solution {
-    class Node {
-        int dist;
-        int val;
-        Node(int dist, int val) {
-            this.dist = dist;
-            this.val = val;
-        }
-    }
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        PriorityQueue<Node> heap = new PriorityQueue<>((a,b) -> {
-            if(a.dist != b.dist) {
-                return Integer.compare(b.dist, a.dist);
-            } 
-            return Integer.compare(b.val, a.val);
-        });
+        int low = 0, high = arr.length-1;
 
-        for(int i=0; i<arr.length; i++) {
-            int dist = Math.abs(arr[i]-x);
-            heap.add(new Node(dist, arr[i]));
-            if(heap.size() > k) {
-                heap.poll();
+        while(high-low >= k) {
+            if(Math.abs(arr[low]-x) > Math.abs(arr[high]-x)) {
+                low++;
+            } else {
+                high--;
             }
         }
 
         List<Integer> res = new ArrayList<>();
 
-        for(int i=0; i<k; i++) {
-            res.add(heap.poll().val);
+        for(int i=low; i<=high; i++) {
+            res.add(arr[i]);
         }
-
-        Collections.sort(res);
 
         return res;
     }
