35f18e09a18d4f707c57f39122871e78cd97f96f
Shalvi Saxena <shalvi.saxena97@live.com>
2025-12-28 16:27:30 -0500

    Time: 9 ms (98.46%) | Memory: 59.8 MB (27.22%) - LeetSync

--- 148-sort-list/sort-list.java
+++ 148-sort-list/sort-list.java
@@ -9,28 +9,47 @@
  * }
  */
 class Solution {
+
+    public ListNode findMid(ListNode head) {
+        ListNode slow = head, fast = head, prev=null;
+        while(fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        prev.next = null;
+        return slow;
+    }
+
+    public ListNode merge(ListNode a, ListNode b) {
+        ListNode dummy = new ListNode(0);
+        ListNode root = dummy;
+
+        while(a!=null && b!=null) {
+            if(a.val < b.val) {
+                dummy.next = a;
+                a = a.next;
+            } else {
+                dummy.next = b;
+                b = b.next;
+            }
+            dummy = dummy.next;
+        }
+
+        dummy.next = a==null? b:a;
+        return root.next;
+    }
+
     public ListNode sortList(ListNode head) {
         if(head == null || head.next == null) {
             return head;
         }
-        PriorityQueue<ListNode> pq = new PriorityQueue<>((a,b)->
-            Integer.compare(a.val, b.val)
-        );
 
-        while(head != null) {
-            pq.add(head);
-            head = head.next;
-        }
-
-        ListNode root = pq.poll();
-        head = root;
-
-        while(!pq.isEmpty()) {
-            root.next = pq.poll();
-            root = root.next;
-        }
-
-        root.next = null;
-        return head;
+        ListNode mid = findMid(head);
+        ListNode left = sortList(mid);
+        ListNode right = sortList(head);
+        
+        return merge(left, right);
     }
 }
