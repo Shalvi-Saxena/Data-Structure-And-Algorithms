15c553bb667357138ed8a5661a7abde16b6978cf
Shalvi Saxena <shalvi.saxena97@live.com>
2026-01-03 15:18:39 -0500

    Time: 0 ms (100.00%) | Memory: 48.4 MB (10.43%) - LeetSync

---
+++ 50-powx-n/powx-n.java
@@ -0,0 +1,21 @@
+class Solution {
+
+    public double myPow(double x, int N) {
+        long n = N;
+        if(n<0) {
+            x = 1/x;
+            n = -n;
+        }
+        double res = 1.0;
+
+        while(n>0) {
+            if((n&1) == 1) {
+                res *= x;
+            }
+            x *= x;
+            n >>= 1;
+        }
+
+        return res;
+    }
+}
