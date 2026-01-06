e8a1b526e5b705b9393fa14a2f38389ce61c4e9e
Shalvi Saxena <shalvi.saxena97@live.com>
2026-01-06 16:17:24 -0500

    Time: 8 ms (77.76%) | Memory: 71 MB (39.94%) - LeetSync

--- 221-maximal-square/maximal-square.java
+++ 221-maximal-square/maximal-square.java
@@ -1,39 +1,21 @@
 class Solution {
-    int m, n;
-    int[][] dp;
-    boolean[][] isVisited;
-
-    public int getMaxSquare(char[][] matrix, int i, int j) {
-        if( i<0 || j<0 ) return 0;
-        if (isVisited[i][j]) return dp[i][j];
-
-        isVisited[i][j] = true;
-
-        if (matrix[i][j] == '0') {
-            dp[i][j] = 0;
-            return 0;
-        }
-        
-        int left = getMaxSquare(matrix, i-1, j);
-        int top = left>0? getMaxSquare(matrix, i, j-1):0;
-        int diag = top>0? getMaxSquare(matrix, i-1, j-1):0;
-        dp[i][j] = 1 + Math.min(left, Math.min(top, diag));
-
-        return dp[i][j];
-    }
     public int maximalSquare(char[][] matrix) {
-        int count = 0;
-        m = matrix.length;
-        n = matrix[0].length;
-        dp = new int[m][n];
-        isVisited = new boolean[m][n];
-
-        for(int i=m-1; i>=0; i--) {
-            for(int j=n-1; j>=0; j--) {
-                int size = getMaxSquare(matrix, i, j);
-                count = Math.max(count, size*size);
+        int count = 0, m = matrix.length, n = matrix[0].length;
+        int[][] dp = new int[m][n];
+        
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                if(matrix[i][j] == '1') {
+                    if(i==0 || j==0) {
+                        dp[i][j] = 1;
+                    } else {
+                        dp[i][j] = 1+Math.min(dp[i-1][j], 
+                                    Math.min(dp[i-1][j-1], dp[i][j-1]));
+                    }
+                    count = Math.max(count, dp[i][j]);
+                }
             }
         }
-        return count;
+        return count*count;
     }
 }
