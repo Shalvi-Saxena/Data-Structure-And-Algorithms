cf5137b8286c891cae567789bfcf8d9cb7cd7cc0
Shalvi Saxena <shalvi.saxena97@live.com>
2026-01-20 08:33:29 -0500

    Time: 7 ms (96.97%) | Memory: 47 MB (86.11%) - LeetSync

--- 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.java
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.java
@@ -1,36 +1,39 @@
 class Solution {
     int m,n;
+    int[][] matrix, grid;
+
     public int longestIncreasingPath(int[][] matrix) {
+        this.matrix = matrix;
         m = matrix.length;
         n = matrix[0].length;
-        int[][] grid = new int[m][n];
+        grid = new int[m][n];
         int ans = 0;
 
         for(int i=0; i<m; i++) {
             for(int j=0; j<n; j++) {
-                ans = Math.max(ans, dfs(matrix, grid, i, j));
+                ans = Math.max(ans, dfs(i, j));
             }
         }
 
         return ans;
     }
 
-    private int dfs(int[][] matrix, int[][] grid, int i, int j) {
+    private int dfs(int i, int j) {
         if(grid[i][j] != 0) return grid[i][j];
 
         int ans = 1;
 
         if(i-1 >= 0 && matrix[i-1][j] < matrix[i][j]) {
-            ans = Math.max(ans, 1+dfs(matrix, grid, i-1, j));
+            ans = Math.max(ans, 1+dfs(i-1, j));
         }
         if(i+1 < m && matrix[i+1][j] < matrix[i][j]) {
-            ans = Math.max(ans, 1+dfs(matrix, grid, i+1, j));
+            ans = Math.max(ans, 1+dfs(i+1, j));
         }
         if(j-1 >= 0 && matrix[i][j-1] < matrix[i][j]) {
-            ans = Math.max(ans, 1+dfs(matrix, grid, i, j-1));
+            ans = Math.max(ans, 1+dfs(i, j-1));
         }
         if(j+1 < n && matrix[i][j+1] < matrix[i][j]) {
-            ans = Math.max(ans, 1+dfs(matrix, grid, i, j+1));
+            ans = Math.max(ans, 1+dfs(i, j+1));
         }
         grid[i][j] = ans;
         return ans;
