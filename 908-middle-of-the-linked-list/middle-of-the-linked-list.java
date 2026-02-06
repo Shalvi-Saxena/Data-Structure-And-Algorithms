5936bb25ccd3879ab3a793d607c80efdee59df8f
Shalvi Saxena <shalvi.saxena97@live.com>
2026-02-06 15:30:14 -0500

    Time: 0 ms (100.00%) | Memory: 43.2 MB (12.74%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.java
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.java
@@ -9,25 +9,18 @@
  * }
  */
 class Solution {
-    int length;
-    public ListNode middleNode(ListNode head, int len) {
-        if(head.next == null) {
-            length = len;
-            return head;
-        }
-
-        ListNode mid = middleNode(head.next, len+1);
-
-        if(len <= Math.ceil(length/2)) {
-            return mid;
-        }
-        return head;
-    }
     public ListNode middleNode(ListNode head) {
         if(head.next == null) {
             return head;
         }
 
-        return middleNode(head, 1);
+        ListNode slow = head, fast = head;
+
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
     }
 }
