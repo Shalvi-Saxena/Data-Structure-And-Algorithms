0409f89952ad2e713d5f81211e741b93d0838b4d
Shalvi Saxena <shalvi.saxena97@live.com>
2026-02-12 10:55:06 -0500

    Time: 15 ms (55.59%) | Memory: 48.8 MB (19.07%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.java
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -1,6 +1,5 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
-        int max = Integer.MIN_VALUE;
         Stack<Integer> st = new Stack<>();
 
         for(int i=nums.length-1; i>=0; i--) {
@@ -8,7 +7,6 @@ class Solution {
                 st.pop();
             }
             st.push(nums[i]);
-            max = Math.max(max, nums[i]);
         }
 
         int[] res = new int[nums.length];
