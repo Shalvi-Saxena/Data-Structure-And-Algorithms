b1f3898074250adb5b8d8d3915a439697d17375c
Shalvi Saxena <shalvi.saxena97@live.com>
2026-02-16 09:43:38 -0500

    Time: 12 ms (58.86%) | Memory: 48.9 MB (82.25%) - LeetSync

--- 744-network-delay-time/network-delay-time.java
+++ 744-network-delay-time/network-delay-time.java
@@ -1,45 +1,35 @@
 class Solution {
-    Map<Integer, List<Pair<Integer, Integer>>> graph;
-    private void bfs(int[] minTime, boolean[] isVisited, int k) {
-        Deque<Integer> q = new ArrayDeque<>();
-        
-        minTime[k] = 0;
-        q.add(k);
-        isVisited[k] = true;
-
-        while(!q.isEmpty()) {
-            int node = q.removeFirst();
-            List<Pair<Integer, Integer>> edges = graph.getOrDefault(node, new ArrayList<>());
-            for(Pair<Integer, Integer> edge: edges) {
-                int dest = edge.getKey();
-                int time = edge.getValue();
-                int arrTime = time+minTime[node];
-
-                if(arrTime < minTime[dest]) {
-                    minTime[dest] = arrTime;
-                    if(!isVisited[dest]) {
-                        q.add(dest);
-                        isVisited[dest] = true;
-                    }                    
-                }
-            }
-            isVisited[node] = false;
-        }
-    }
     public int networkDelayTime(int[][] times, int n, int k) {
-        graph = new HashMap<>();
+        List<List<int[]>> graph = new ArrayList<>();
+
+        for(int i=0; i<=n; i++) {
+            graph.add(new ArrayList<>());
+        }
 
         for(int[] edge: times) {
-            graph.putIfAbsent(edge[0], new ArrayList<>());
-            graph.get(edge[0]).add(new Pair(edge[1], edge[2]));
+            graph.get(edge[0]).add(new int[]{edge[1], edge[2]});
         }
 
         int[] minTime = new int[n+1];
-        boolean[] isVisited = new boolean[n+1]; 
-
         Arrays.fill(minTime, Integer.MAX_VALUE);
-        bfs(minTime, isVisited, k);
+        minTime[k] = 0;
+        
+        Deque<Integer> q = new ArrayDeque<>();
+        q.add(k);
 
+        while(!q.isEmpty()) {
+            int node = q.removeFirst();
+            List<int[]> edges = graph.get(node);
+            for(int[] edge: edges) {
+                int arrTime = minTime[node]+edge[1];
+                if(arrTime < minTime[edge[0]]) {
+                    minTime[edge[0]] = arrTime;
+                    q.add(edge[0]);
+                }
+            }
+
+        }
+        
         int totalTime = Integer.MIN_VALUE;
 
         for(int i=1; i<=n; i++) {
