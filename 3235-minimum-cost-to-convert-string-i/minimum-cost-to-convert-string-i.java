63fae4895d45a83402319ab7e2a16a1c434f693e
Shalvi Saxena <shalvi.saxena97@live.com>
2026-02-25 21:23:04 -0500

    Time: 12 ms (99.48%) | Memory: 48 MB (71.91%) - LeetSync

--- 3235-minimum-cost-to-convert-string-i/minimum-cost-to-convert-string-i.java
+++ 3235-minimum-cost-to-convert-string-i/minimum-cost-to-convert-string-i.java
@@ -1,10 +1,9 @@
 class Solution {
     public long minimumCost(String source, String target, char[] original, char[] changed, int[] cost) {
         int[][] min = new int[26][26];
-        int inf = Integer.MAX_VALUE;
 
         for(int i=0; i<26; i++) {
-            Arrays.fill(min[i], inf);
+            Arrays.fill(min[i], Integer.MAX_VALUE);
             min[i][i] = 0;
         }
 
@@ -16,9 +15,9 @@ class Solution {
 
         for(int k=0; k<26; k++) {
             for(int i=0; i<26; i++) {
-                if(min[i][k] == inf)    continue;
+                if(min[i][k] == Integer.MAX_VALUE)    continue;
                 for(int j=0; j<26; j++) {
-                    if(min[k][j] == inf)    continue;
+                    if(min[k][j] == Integer.MAX_VALUE)    continue;
                     min[i][j] = Math.min(min[i][k]+min[k][j], min[i][j]);
                 }
             }
@@ -28,7 +27,7 @@ class Solution {
 
         for(int i=0; i<source.length(); i++) {
             int s = source.charAt(i)-'a', t = target.charAt(i)-'a';
-            if(min[s][t] == inf) {
+            if(min[s][t] == Integer.MAX_VALUE) {
                 return -1;
             }
             c += min[s][t];
