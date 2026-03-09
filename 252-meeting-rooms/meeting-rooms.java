ba18e5cdd9adb33bc4742ed4a468bdc1744b5cf9
Shalvi Saxena <shalvi.saxena97@live.com>
2026-03-09 13:41:05 -0400

    Time: 6 ms (90.38%) | Memory: 48 MB (95.72%) - LeetSync

--- 252-meeting-rooms/meeting-rooms.java
+++ 252-meeting-rooms/meeting-rooms.java
@@ -1,5 +1,7 @@
 class Solution {
     public boolean canAttendMeetings(int[][] intervals) {
+        if(intervals.length <= 1)   return true;
+
         Arrays.sort(intervals, (a,b)->a[0]-b[0]);
 
         for(int i=1;i<intervals.length;i++){
