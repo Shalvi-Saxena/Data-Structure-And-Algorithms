9ffbf1f06d421e0b25b737950f4d6481e6a19b15
Shalvi Saxena <shalvi.saxena97@live.com>
2026-03-09 13:40:30 -0400

    Time: 8 ms (18.18%) | Memory: 48.8 MB (7.19%) - LeetSync

--- 252-meeting-rooms/meeting-rooms.java
+++ 252-meeting-rooms/meeting-rooms.java
@@ -1,24 +1,11 @@
 class Solution {
     public boolean canAttendMeetings(int[][] intervals) {
-        if(intervals.length <= 1)   return true;
+        Arrays.sort(intervals, (a,b)->a[0]-b[0]);
 
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b)-> a[0]-b[0]);
-        for(int[] interval: intervals) {
-            pq.add(interval);
-        }
-
-        int[] meet = pq.poll();
-
-        while(!pq.isEmpty()) {
-            int[] nextMeet = pq.poll();
-
-            if(nextMeet[0] < meet[1]) {
+        for(int i=1;i<intervals.length;i++){
+            if(intervals[i][0] < intervals[i-1][1])
                 return false;
-            }
-
-            meet = nextMeet;
         }
-
         return true;
     }
 }
